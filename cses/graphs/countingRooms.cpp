0087c090ded7f55981980bd620d978675486095b
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-10-20 11:49:10 +0530

    Algorithm Used - DFS

--- cses/graphs/countingRooms.cpp
+++ cses/graphs/countingRooms.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #..#...#
 ########
 */
-
+ 
 bool isValid(int i, int j, int n, int m){
     return (i>=0 && i<n && j>=0 && j<m);
 }
@@ -24,5 +24,32 @@ int main(){
             cin>>v[i][j];
         }
     }
+    int rooms = 0;
+    int l = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(!vis[i][j] && v[i][j] == '.'){
+                rooms++;
+                stack<pair<int, int>> s;
+                s.push(make_pair(i,j));
+                while(!s.empty()){
+                    pair<int, int> cur = s.top();
+                    // cout<<cur.first<<" "<<cur.second<<endl;
+                    s.pop();
+                    if(!vis[cur.first][cur.second]){
+                        vis[cur.first][cur.second] = true;
+                    }
+                    for(int k=0; k<4; k++){
+                        pair<int, int> next = make_pair(cur.first + dx[k], cur.second + dy[k]);
+                        if(isValid(next.first, next.second, n, m) && !vis[next.first][next.second] && v[next.first][next.second] == '.'){
+                            s.push(make_pair(next.first, next.second));
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+    cout<<rooms<<endl;
     return 0;
 }
