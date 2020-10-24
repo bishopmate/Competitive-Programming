155524ccf0246add51900dbf77eff212ddfa712e
Sashi Bhushan A Rajput <sam556913@gmail.com>
2020-10-24 11:50:17 +0530

    Replaced Euclid's GCD Algo with Binary Variant

--- template.cpp
+++ template.cpp
@@ -43,12 +43,26 @@ bool isOverflow(int a, int b){
    
     return false; 
 }
-int gcd (int a, int b) {
-    while (b) {
-        a %= b;
-        swap(a, b);
-    }
-    return a;
+int gcdBinary(int a, int b, int res){
+  if(a == b){
+    return a * res;
+  }
+  if(a == 0){
+    return b*res;
+  }else if(b == 0){
+    return a*res;
+  }
+  if(!(a&1) && !(b&1)){
+    return gcdBinary((a >> 1), (b >> 1), res*2);
+  }else if(!(a&1)){
+    return gcdBinary((a >> 1), b, res);
+  }else if(!(b&1)){
+    return gcdBinary(a, (b >> 1), res);
+  }else if(a > b){
+    return gcdBinary(a-b, b, res);
+  }else{
+    return gcdBinary(a, b-a, res);
+  }
 }
 int powerer(int a,int b){
     int ans=1;
